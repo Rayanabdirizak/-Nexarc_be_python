f5b4959171a5f9fde4d98543022b9954ff30ec1e
Rayan <bainanceb8@gmail.com>
2026-04-06 11:41:53 +0300

    hours_to_seconds

---
+++ finance_calculator.py
@@ -0,0 +1,5 @@
+monthly  = 5000
+expenses = 4000
+savings = 1000
+savings = savings * 12 + (savings * 12 * 0.05)
+print(savings)
---
+++ hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+minutes = 60 * hours
+seconds = 60 * minutes
+print(hours,seconds)
